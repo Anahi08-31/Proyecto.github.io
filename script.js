f7ede1928d394441e57e6bb50a6104f93efde95b
Anahi08-31 <123321123+Anahi08-31@users.noreply.github.com>
2023-02-27 21:00:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+let respuesta=prompt(`Buen dia,¿Me dirias tu nombre?`);	
+if(respuesta != null){
+alert(`Muy bien ${respuesta} ,dejame contarte sobre mi`);
+}else {if(respuesta == null)
+{alert(`Es una pena`);}}
+
+
+
+function confirmar(){
+    onClick = confirm("Estas seguro de enviar este formulario?");
+   }
+
